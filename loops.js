e7269cb572c477cc0271c95ab26f1a04c37ca1be
Melissa Heying <melissakheying@gmail.com>
2020-05-17 14:01:16 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,10 +1,10 @@
-const array = []
-array.length = 25
+const array = [];
+array.length = 25;
 function forLoop(array) {
   for (let i = 0; i < 25; i++){
     if (i === 1) {
     array.push("I am 1 strange loop.");
   } else {
-  console.log(`"I am ${i} strange loops."`);
+  array.push(`"I am ${i} strange loops."`);
 }}
 }
