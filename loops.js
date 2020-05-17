9c8ae5a6b7738b0da33f735b72f60c415cc368e0
Melissa Heying <melissakheying@gmail.com>
2020-05-17 14:03:45 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,5 +1,5 @@
 
-function forLoop(array) {
+/*function forLoop(array) {
   for (let i = 0; i < 25; i++){
     if (i === 1) {
     array.push("I am 1 strange loop.")
@@ -7,4 +7,14 @@ function forLoop(array) {
   array.push(`I am ${i} strange loops.`)
 }}
 retun array;
+}*/
+function forLoop(array) {
+   for ( let i = 0; i < 25; i++) {
+     if (i === 1) {
+       array.push(`I am 1 strange loop.`)
+     } else {
+       array.push(`I am ${i} strange loops.`)
+     }
+  }
+  return array;
 }
