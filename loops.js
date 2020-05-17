6ccaffe088e7c28676b961f23b4cb4ff9c3b3f98
Melissa Heying <melissakheying@gmail.com>
2020-05-17 13:37:13 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,9 +1,9 @@
-
+const array = []
 
 function forLoop(array) {
   for (let i = 0; i < 25; i++){
     if (i === 1) {
-    array.slice("I am 1 strange loop.")
+    array.push("I am 1 strange loop.")
   } else {
   array.slice(`I am ${i} strange loops.`)
 }}
